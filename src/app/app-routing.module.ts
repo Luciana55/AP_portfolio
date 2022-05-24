beafeba9e4b8cb937e045366cf2a2955714c524b
LucianaV <vidallucianam@gmail.com>
2022-05-23 23:43:17 -0300

    navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PersonaComponent } from './components/persona/persona.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { SkillsComponent } from './components/skills/skills.component';
 
 const routes: Routes = [
 {path: 'header', component: HeaderComponent},
 {path: 'persona', component: PersonaComponent},
-{path: 'skills', component: SkillsComponent}
+{path: 'skills', component: SkillsComponent},
+{path: 'proyectos', component: ProyectosComponent},
+{path: 'experiencia', component: ExperienciaComponent},
+{path: 'educacion', component: EducacionComponent}
 
 ];
 
