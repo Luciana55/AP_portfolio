f8a09f557d6b04a4d9293b7f8c432df7ad7f198f
LucianaVidal <vidallucianam@gmail.com>
2022-05-28 00:25:20 -0300

    servicios

--- src/app/service/header.service.ts
+++ src/app/service/header.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Persona } from '../components/persona/persona';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeaderService {
+  private apiServerUrl=environment.apiBaseUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getPersona(): Observable<Persona[]> {
+    return this.http.get<Persona[]>(`${this.apiServerUrl}/persona/all`);
+  }
+
+  public addPersona(persona: Persona): Observable<Persona> {
+    return this.http.post<Persona>(`${this.apiServerUrl}/persona/add`, persona);
+  }
+
+  public updatePersona(persona: Persona): Observable<Persona> {
+    return this.http.put<Persona>(`${this.apiServerUrl}/persona/update`, persona);
+  }
+
+  public deletePersona(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/persona/delete/${id}`);
+  }
+
 }
--- src/app/service/skills.service.ts
+++ src/app/service/skills.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Skills } from '../components/skills/skills';
 
 @Injectable({
   providedIn: 'root'
@@ -10,4 +12,21 @@ export class SkillsService {
   private apiServerUrl = environment.apiBaseUrl;
 
   constructor(private http:HttpClient) { }
+
+  public getSkills(): Observable<Skills[]> {
+    return this.http.get<Skills[]>(`${this.apiServerUrl}/skills/all`);
+  }
+
+  public addSkills(skills: Skills): Observable<Skills> {
+    return this.http.post<Skills>(`${this.apiServerUrl}/skills/add`, skills);
+  }
+
+  public updateSkills(skills: Skills): Observable<Skills> {
+    return this.http.put<Skills>(`${this.apiServerUrl}/skills/update`, skills);
+  }
+
+  public deleteSkills(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/skills/delete/${id}`);
+  }
+
 }
